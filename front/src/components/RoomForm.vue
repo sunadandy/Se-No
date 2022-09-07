e5067353e9bf72b5437d0fecbb4520901c8a6606
sunadandy <46664550+sunadandy@users.noreply.github.com>
2022-09-07 13:54:48 +0900

    [update]Change MaxCapacity from 10 to 30

--- front/src/components/RoomForm.vue
+++ front/src/components/RoomForm.vue
@@ -20,7 +20,7 @@ export default {
         return {
             room_name: null,
             room_capa: null,
-            items: [1,2,3,4,5,6,7,8,9,10],
+            items: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
         }
     },
     methods: {
